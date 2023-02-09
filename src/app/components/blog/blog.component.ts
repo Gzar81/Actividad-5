faed98190b7529357e0ab5c28483727a600c127e
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-09 19:20:52 +0100

    todo funciona

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -15,7 +15,7 @@ export class BlogComponent {
       title: new FormControl('',[
         Validators.required,
         Validators.minLength(3),
-        Validators.maxLength(30)
+        Validators.maxLength(20)
       ]),
       url: new FormControl('', [
         Validators.required,
@@ -28,13 +28,13 @@ export class BlogComponent {
       ]),
       date: new FormControl('',[
         Validators.required,
-        Validators.minLength(6)
+        Validators.pattern(/^\d{2}\/\d{2}\/\d{2}$/)
       ])
     }, [])
 
     this.arrUsuarios = [
-      {title: "BLOOD", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "14/02/23"},
-      {title: "MORTAL PUNCH", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: "The realms couldn't hold Mortal Kombat. It quickly grew out of the realms and onto your shelves.", date: "15/02/23"}
+      {title: "MK SERVERS DOWN", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: "We're aware players are currently unable to connect to servers in Mortal Kombat 11 and can confirm this is being investigated. We'll share an update here when this issue has been resolved. We appreciate your patience.", date: "14/02/23"},
+      {title: "WE ARE BACK ONLINE", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "15/02/23"}
     ]
 
   }
