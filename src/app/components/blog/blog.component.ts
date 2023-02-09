cf9ae7ec829ce9fe4ee06413a69e2f556bfc9eb2
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-09 12:55:34 +0100

    maquetación parcial derecha

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -33,8 +33,8 @@ export class BlogComponent {
     }, [])
 
     this.arrUsuarios = [
-      {title: "Blood", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "04/06/23"},
-      {title: "Mortal Punch", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: "The realms couldn't hold Mortal Kombat. It quickly grew out of the realms and onto your shelves.", date: "04/06/23"}
+      {title: "BLOOD", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "14/02/23"},
+      {title: "MORTAL PUNCH", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: "The realms couldn't hold Mortal Kombat. It quickly grew out of the realms and onto your shelves.", date: "15/02/23"}
     ]
 
   }
