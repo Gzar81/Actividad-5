317f68ca0ee317b7be74c5644f8f9da90a1be837
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-11 20:55:32 +0100

    corregido y limpiado el código

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -32,18 +32,18 @@ export class BlogComponent {
       ])
     }, [])
 
-    this.arrUsuarios = [
+    this.arrNews = [
       {title: "MK SERVERS DOWN", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: "We're aware players are currently unable to connect to servers in Mortal Kombat 11 and can confirm this is being investigated. We'll share an update here when this issue has been resolved. We appreciate your patience.", date: "14/02/23"},
       {title: "WE ARE BACK ONLINE", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "15/02/23"}
     ]
 
   }
-  arrUsuarios: News[] = [];
+  arrNews: News[] = [];
 
 
   recogerDatosForm(){
-    let usuario = this.miFormulario.value;
-    this.arrUsuarios.push(usuario);
+    let news = this.miFormulario.value;
+    this.arrNews.push(news);
     this.miFormulario.reset();
          
   }
