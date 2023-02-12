5d107c0a0d61b7d42a041ea4e43d54e2abd54a4c
Gzar81 <116944846+Gzar81@users.noreply.github.com>
2023-02-12 20:10:40 +0100

    100% revisado, ordenado y limpiado

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -11,9 +11,9 @@ export class BlogComponent {
   miFormulario: FormGroup;
   buttonSubmitTouched: boolean = false;
 
-  constructor(){    //recibe un {objeto con todo el contenido de los campos del formulario}, y un [array de validaciones] 
+  constructor() {
     this.miFormulario = new FormGroup({
-      title: new FormControl('',[
+      title: new FormControl('', [
         Validators.required,
         Validators.minLength(3),
         Validators.maxLength(20)
@@ -27,30 +27,30 @@ export class BlogComponent {
         Validators.minLength(30),
         Validators.maxLength(300)
       ]),
-      date: new FormControl('',[
+      date: new FormControl('', [
         Validators.required
       ])
     }, [])
 
     this.arrNews = [
-      {title: "MK SERVERS DOWN", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: "We're aware players are currently unable to connect to servers in Mortal Kombat 11 and can confirm this is being investigated. We'll share an update here when this issue has been resolved. We appreciate your patience.", date: "2023-02-14"},
-      {title: "WE ARE BACK ONLINE", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "2023-02-15"}
+      { title: "MK SERVERS DOWN", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFmSvkLdaUAEjSKZ.jpg?w=400&func=cover&ci_url_encoded=1", text: "We're aware players are currently unable to connect to servers in Mortal Kombat 11 and can confirm this is being investigated. We'll share an update here when this issue has been resolved. We appreciate your patience.", date: "2023-02-12" },
+      { title: "WE ARE BACK ONLINE", url: "https://cdn.taggbox.com/v7/https%3A%2F%2Fpbs.twimg.com%2Fmedia%2FFiMXQMZVQAAYvLj.jpg?w=400&func=cover&ci_url_encoded=1", text: " All platforms are now back online. If players resume a previous MK11 session from a standby state on consoles, they may need to relaunch the game to connect.", date: "2023-02-13" }
     ]
 
   }
   arrNews: News[] = [];
 
 
-  recogerDatosForm(){
+  recogerDatosForm() {
 
     this.buttonSubmitTouched = true;
 
     if (this.miFormulario.valid) {
-    let news = this.miFormulario.value;
-    this.arrNews.push(news);
-    this.miFormulario.reset(this.buttonSubmitTouched = false);
-  }
-         
+      let news = this.miFormulario.value;
+      this.arrNews.push(news);
+      this.miFormulario.reset(this.buttonSubmitTouched = false);
+    }
+
   }
   titleRequiredError() {
 
@@ -59,24 +59,24 @@ export class BlogComponent {
     return (title.touched || this.buttonSubmitTouched) && title.hasError('required');
   }
 
-  mintitleLengthError(){
+  mintitleLengthError() {
     const title = this.miFormulario.controls['title'];
 
     return title.dirty && title.hasError('minlength');
   }
 
-  maxtitleLengthError(){
+  maxtitleLengthError() {
     const title = this.miFormulario.controls['title'];
 
     return title.dirty && title.hasError('maxlength');
   }
 
-  urlRequiredError(){
+  urlRequiredError() {
     const url = this.miFormulario.controls['url'];
 
     return (url.touched || this.buttonSubmitTouched) && url.hasError('required');
   }
-  urlPatternError(){
+  urlPatternError() {
     const url = this.miFormulario.controls['url'];
 
     return url.dirty && url.hasError('pattern');
@@ -89,37 +89,32 @@ export class BlogComponent {
     return (text.touched || this.buttonSubmitTouched) && text.hasError('required');
   }
 
-  mintextLengthError(){
+  mintextLengthError() {
     const text = this.miFormulario.controls['text'];
 
     return text.dirty && text.hasError('minlength');
   }
 
-  maxtextLengthError(){
+  maxtextLengthError() {
     const text = this.miFormulario.controls['text'];
 
     return text.dirty && text.hasError('maxlength');
   }
 
-  dateRequiredError(){
+  dateRequiredError() {
     const date = this.miFormulario.controls['date'];
 
     return (date.touched || this.buttonSubmitTouched) && date.hasError('required');
   }
 
-  /* dateCurrentError() {
-    const date = this.miFormulario.controls['date'].value;
-    const currentDate = new Date().toISOString().substring(0, 10);
-    return this.miFormulario.controls['date'].dirty && date !== currentDate;
-  } */
   dateCurrentError() {
     const date = this.miFormulario.controls['date'].value;
     const currentDate = new Date().toISOString().substring(0, 10);
     const invalidDate = date !== currentDate;
     this.miFormulario.controls['date'].setErrors(invalidDate ? { 'invalidDate': true } : null);
-    return (this.miFormulario.controls['date'].dirty || this.buttonSubmitTouched) &&  invalidDate;
+    return (this.miFormulario.controls['date'].dirty || this.buttonSubmitTouched) && invalidDate;
   }
-  
-  
-  
+
+
+
 }
